f7e0f001968e9dbed636f36b14d50d86a2aa725b
Paolo <jplojo34@gmail.com>
2021-08-20 14:25:09 +0800

    static layout elements

--- app/src/main/java/com/mobdeve/mco/MainActivity.java
+++ app/src/main/java/com/mobdeve/mco/MainActivity.java
@@ -1,14 +1,29 @@
 package com.mobdeve.mco;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView nav;
+    FloatingActionButton fab_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nav = findViewById(R.id.bottom_nav);
+        fab_add = findViewById(R.id.fab_add);
+
     }
 }
