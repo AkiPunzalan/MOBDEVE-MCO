0da5109c1e5b27902df96965826232c6d83a97c9
Paolo <jplojo34@gmail.com>
2021-09-15 13:08:47 +0800

    removed Metrics feature

--- app/src/main/java/com/mobdeve/mco/Fragments/MetricsFragment.java
+++ app/src/main/java/com/mobdeve/mco/Fragments/MetricsFragment.java
@@ -73,53 +73,53 @@ public class MetricsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_metrics, container, false);
 
-        weekChart = v.findViewById(R.id.chart1);
-
-        ArrayList<BarEntry> taskdata = new ArrayList<>();
-        taskdata.add(new BarEntry(0, 12));
-        taskdata.add(new BarEntry(1, 13));
-        taskdata.add(new BarEntry(2, 20));
-        taskdata.add(new BarEntry(3, 0));
-        taskdata.add(new BarEntry(4, 7));
-        taskdata.add(new BarEntry(5, 12));
-        taskdata.add(new BarEntry(6, 22));
-
-        BarDataSet taskdataset = new BarDataSet(taskdata, "Days most active");
-        BarData bardata = new BarData(taskdataset);
-
-        weekChart.setData(bardata);
-        weekChart.getAxisLeft().setTextColor(getResources().getColor(R.color.white)); // left y-axis
-        weekChart.getXAxis().setTextColor(getResources().getColor(R.color.white));
-        weekChart.getLegend().setTextColor(getResources().getColor(R.color.white));
-        weekChart.getDescription().setTextColor(getResources().getColor(R.color.white));
-
-        weekChart.getAxisLeft().setDrawGridLines(false);
-        weekChart.getAxisRight().setDrawGridLines(false);
-        weekChart.getXAxis().setDrawGridLines(false);
-
-        weekChart.getDescription().setText("Days most active");
-
-        ArrayList<String> xLabel = new ArrayList<>();
-        xLabel.add("Mon");
-        xLabel.add("Tue");
-        xLabel.add("Wed");
-        xLabel.add("Thu");
-        xLabel.add("Fri");
-        xLabel.add("Sat");
-        xLabel.add("Sun");
-
-        XAxis xAxis = weekChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
-
-        ValueFormatter formatter = new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value) {
-                return xLabel.get((int) value);
-            }
-        };
-
-        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
-        xAxis.setValueFormatter(formatter);
+//        weekChart = v.findViewById(R.id.chart1);
+//
+//        ArrayList<BarEntry> taskdata = new ArrayList<>();
+//        taskdata.add(new BarEntry(0, 12));
+//        taskdata.add(new BarEntry(1, 13));
+//        taskdata.add(new BarEntry(2, 20));
+//        taskdata.add(new BarEntry(3, 0));
+//        taskdata.add(new BarEntry(4, 7));
+//        taskdata.add(new BarEntry(5, 12));
+//        taskdata.add(new BarEntry(6, 22));
+//
+//        BarDataSet taskdataset = new BarDataSet(taskdata, "Days most active");
+//        BarData bardata = new BarData(taskdataset);
+//
+//        weekChart.setData(bardata);
+//        weekChart.getAxisLeft().setTextColor(getResources().getColor(R.color.white)); // left y-axis
+//        weekChart.getXAxis().setTextColor(getResources().getColor(R.color.white));
+//        weekChart.getLegend().setTextColor(getResources().getColor(R.color.white));
+//        weekChart.getDescription().setTextColor(getResources().getColor(R.color.white));
+//
+//        weekChart.getAxisLeft().setDrawGridLines(false);
+//        weekChart.getAxisRight().setDrawGridLines(false);
+//        weekChart.getXAxis().setDrawGridLines(false);
+//
+//        weekChart.getDescription().setText("Days most active");
+//
+//        ArrayList<String> xLabel = new ArrayList<>();
+//        xLabel.add("Mon");
+//        xLabel.add("Tue");
+//        xLabel.add("Wed");
+//        xLabel.add("Thu");
+//        xLabel.add("Fri");
+//        xLabel.add("Sat");
+//        xLabel.add("Sun");
+//
+//        XAxis xAxis = weekChart.getXAxis();
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+//
+//        ValueFormatter formatter = new ValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value) {
+//                return xLabel.get((int) value);
+//            }
+//        };
+//
+//        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+//        xAxis.setValueFormatter(formatter);
 
         return v;
     }
