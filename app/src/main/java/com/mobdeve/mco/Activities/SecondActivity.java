2da86be2f11ac8c0c2ac749f3712dffcc9780b5e
AkiPunzalan <antonio_punzalan@dlsu.edu.ph>
2021-09-17 10:37:18 +0800

    finished LoginActivity

---
+++ app/src/main/java/com/mobdeve/mco/Activities/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.mco.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobdeve.mco.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
